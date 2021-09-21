5c509872bef4eef6f8640bfdecdbca54c6fc7cba
katalin-kiss <to.katalin.kiss@gmail.com>
2021-09-21 13:19:06 +0200

    Initial commit + dependencies
    Initial commit from create-next-app,
    dependencies, color variables added

--- next.config.js
+++ next.config.js
@@ -1,3 +1,11 @@
 module.exports = {
   reactStrictMode: true,
-}
+  env: {
+    SANITY_DATASET_NAME: process.env.SANITY_DATASET_NAME,
+    SANITY_PROJECT_ID: process.env.SANITY_PROJECT_ID,
+  },
+  images: {
+    domains: ['cdn.sanity.io'],
+  },
+  productionBrowserSourceMaps: true,
+};
